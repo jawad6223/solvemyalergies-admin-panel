9cfde766b5618c13dda526860f2b7f73002c22be
Muhammad Noman <jawad.bdo99@gmail.com>
2025-09-10 14:12:55 +0500

    push code

--- src/components/StoreAnalytics/Sales.tsx
+++ src/components/StoreAnalytics/Sales.tsx
@@ -21,30 +21,39 @@ const Sales: React.FC<SalesProps> = ({ searchTerm }) => {
 
     return (
         <div className="flex items-center gap-4 mt-4">
-            <div className="lg:w-[80%] flex items-center rounded-lg border border-[#CCCCCC] shadow-sm">
+            <div className="lg:w-[75%] py-[20px] flex items-center rounded-lg border border-[#CCCCCC] shadow-sm">
                 {filteredData.map((item, idx) => (
                     <div
                         key={idx}
-                        className={`px-[37px] py-[20px] flex flex-col justify-between ${idx === 0 ? "border-r border-[#B1A9A9]" : ""}`}
+                        className={`pl-[37px] pr-[120px] flex flex-col justify-between ${(idx === 0 || idx === 1) ? "border-r border-[#B1A9A9]" : ""}`}
                     >
                         <div className="flex flex-col gap-2">
                             {item.image && (
-                                <div className={`bg-[#FDF2EE] rounded-full px-[13px] py-[10px] ${idx === 0 ? " " : ""}`}>
+                                <div
+                                    className={`rounded-full px-[13px] py-[10px] w-fit ${idx === 0
+                                        ? "bg-[#FDF2EE]"
+                                        : idx === 1
+                                            ? "bg-[#E9F3FA]"
+                                            : idx === 2
+                                                ? "bg-[#F5E4E4]"
+                                                : ""
+                                        }`}
+                                >
                                     <Image src={item.image} alt={item.sales} width={16} height={20} />
                                 </div>
                             )}
-                            <span className="font-semibold">{item.sales}</span>
-                            <span
-                                className={`text-sm ${item.icon === IoArrowDownOutline
-                                    ? "text-red-500"
-                                    : "text-green-500"
-                                    } flex items-center gap-1`}
-                            >
-                                <item.icon /> {item.number}
-                            </span>
+                            <div className="flex items-center gap-2">
+                                <p className="font-normal text-[#999999]">{item.sales}</p>
+                                <div className="flex items-center gap-0.5">
+                                    <p className={`font-medium text-[14px] ${(idx === 0 || idx === 1) ? "text-[#21BA45]" : idx === 2 ? "text-[#F2711C]" : ""}`}>{item.number}</p>
+                                    <p className={`font-medium text-[14px] ${(idx === 0 || idx === 1) ? "text-[#21BA45]" : idx === 2 ? "text-[#F2711C]" : ""}`}><item.icon /></p>
+                                </div>
+                            </div>
+                            <div>
+                                <div className="text-[24px] text-[#222222] font-semibold">{item.rating}</div>
+                                <div className="text-[#1E1E1E] text-[14px] font-normal">{item.text}</div>
+                            </div>
                         </div>
-                        <div className="text-2xl font-bold">{item.rating}</div>
-                        <div className="text-gray-500 text-sm">{item.text}</div>
                     </div>
                 )
                 )}
